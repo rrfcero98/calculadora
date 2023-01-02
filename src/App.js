6d0be6da91d888f380899ae2cb4fdab1bed87f96
Rafael Ceron <rrfcero98@gmail.com>
2023-01-02 08:06:54 -0600

    Agregando estructura incial al logo y primeros estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
     </div>
   );
 }
