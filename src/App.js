73f2c0bb2915c7cda4ba1cfd930bc7c9f6e2df4c
Rafael Ceron <rrfcero98@gmail.com>
2023-01-03 14:18:23 -0600

    Agregando boton de clear junto con sus estilos y los ultimos estilos de la pantalla

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-import Boton from './componentes/Boton';
-import Pantalla from './componentes/Pantalla';
+import "./App.css";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+import Boton from "./componentes/Boton";
+import Pantalla from "./componentes/Pantalla";
+import BotonClear from "./componentes/BotonClear";
 
 function App() {
   return (
     <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
+      <div className="freecodecamp-logo-contenedor">
+        <img
           src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          alt='Logo de freeCodeCamp'
+          className="freecodecamp-logo"
+          alt="Logo de freeCodeCamp"
         />
       </div>
-      <div className='contenedor-calculadora'>
-        <div className='fila'>
-          <Pantalla />
-        </div>
-        <div className='fila'>
+      <div className="contenedor-calculadora">
+        <Pantalla />
+        <div className="fila">
           <Boton>1</Boton>
           <Boton>2</Boton>
           <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <Boton>4</Boton>
           <Boton>5</Boton>
           <Boton>6</Boton>
           <Boton>-</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <Boton>7</Boton>
           <Boton>8</Boton>
-          <Boton>9</Boton>          
+          <Boton>9</Boton>
           <Boton>x</Boton>
         </div>
-        <div className='fila'>
+        <div className="fila">
           <Boton>=</Boton>
           <Boton>0</Boton>
           <Boton>.</Boton>
           <Boton>/</Boton>
         </div>
-        <div className='fila'>
-          
+        <div className="fila">
+          <BotonClear>Clear</BotonClear>
         </div>
       </div>
     </div>
